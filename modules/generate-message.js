f35423b1d28f67e8715f2d7dbccc657a5d12237f
franknyro <franknyrojp@gmail.com>
2020-10-12 15:31:55 +0900

    時差ボケを修正

--- modules/generate-message.js
+++ modules/generate-message.js
@@ -2,38 +2,38 @@
 
 // 0 late-night 4 early-morning 9 morning 12 afternoon 15 early-evening 18 evening 21 night 24
 const UTC = new Date();
-const JST = new Date(UTC.getTime() + 32400000);
-const curHours = JST.getUTCHours();
+const JSTHours = UTC.getUTCHours();
+JSTHours = (JSTHours + 9) % 24;
 
 // あいさつ（優先度高）時間帯によって返答が変わる
 // ねぎらいのことば（優先度低）時間帯によって返答が変わらない
 module.exports = (msg) => {
     if (Math.floor(Math.random() * msg.member.voice.channel.members.size) === 0) {
         if (msg.content.match(/\bo(ha|ah)|(お|オ)(は|ハ)|ｵﾊ/i)) {
-            if (curHours < 4) {
+            if (JSTHours < 4) {
                 return 'zzz...';
             }
-            if (curHours < 9) {
+            if (JSTHours < 9) {
                 return 'オハヨウゴザイマス ハヤイデス？';
             }
-            if (curHours < 12) {
+            if (JSTHours < 12) {
                 return 'オハヨウゴザイマス';
             }
-            if (curHours < 15) {
+            if (JSTHours < 15) {
                 return 'モウ オヒルデスカ？';
             }
-            if (curHours < 18) {
+            if (JSTHours < 18) {
                 return 'オヒルネ シテイラッシャイマス？';
             }
-            if (curHours < 21) {
+            if (JSTHours < 21) {
                 return 'イマ ヨルデス...';
             }
-            if (curHours < 24) {
+            if (JSTHours < 24) {
                 return 'ネボケテマスカ？';
             }
         }
         if (msg.content.match(/\bkon|^((こ|コ)(ん|ン)|ｺﾝ)|\b(o|u)i*-*s+u|^((お|オ|う|ウ)(い|イ|ぃ|ィ)*ー*(っ|ッ)*(す|ス)|(ｵ|ｳ)(ｲ|ｨ)*ｰ*ｯ*?ｽ|押忍)|^((ち|チ)(わ|ワ|ゎ|ヮ|は|ハ|ゃ|ャ)|ﾁ(ﾜ|ﾊ|ｬ))|\bdou?mo|^((ど|ド)(う|ウ)?(も|モ)|ﾄﾞｳ?ﾓ)|^((や|ヤ|ゃ|ャ)(あ|ア|ぁ|ァ)|(ﾔ|ｬ)(ｱ|ｧ))/i)) {
-            if (4 <= curHours && curHours < 18) {
+            if (4 <= JSTHours && JSTHours < 18) {
                 return 'コンニチワ';
             }
             else {
@@ -47,10 +47,10 @@ module.exports = (msg) => {
             return 'オヤスミナサイ';
         }
         if (msg.content.match(/\bnemu|(ね|ネ)(む|ム)|ﾈﾑ|眠た?い|(ね|寝)たい/i)) {
-            if (4 <= curHours && curHours < 12) {
+            if (4 <= JSTHours && JSTHours < 12) {
                 return 'ネブソク デス？';
             }
-            if (12 <= curHours && curHours < 18) {
+            if (12 <= JSTHours && JSTHours < 18) {
                 return 'オヒルネ シマショウ';
             }
             return 'ソロソロ ネマショウ';
